dabb996b6ad3c57278c0706018d85e60158ef78c
Siyan Guo <siyang@Hey-There.local>
2021-09-18 22:28:31 -0400

    add Comment Post User models

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+
+class User extends Model {
+
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+ };
+
+//define table columns and configuration
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the password must be at least six characters long
+                len: [6]
+            }
+        }
+    },
+    {
+        hooks: {
+            //hashing the password before create User
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    },
+);
+
+module.exports = User;
